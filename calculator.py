4e35d45125f600499d42fc9b481f65d6c594a3d6
Your Name <your@email.com>
2025-11-04 23:15:59 +0530

    initial commit

---
+++ calculator.py
@@ -0,0 +1,34 @@
+import tkinter
+
+button_values = [
+    ["AC","+/-","%","/"],
+    ["7","8","9","*"],
+    ["4","5","6","-"],
+    ["1","2","3","+"],
+    ["0",".","="]
+]
+
+right_symbols = ["+","-","*","/","="]
+top_symbols = ["AC","+/-","%"]
+
+row_count = len(button_values)
+column_count = len(button_values[0])
+
+color_light_gray = "#D4D4D2"
+color_black = "#1C1C1C"
+color_dark_gray = "#505050"
+color_orange = "#FF9500"
+color_white = "white"
+
+window =tkinter.Tk()
+window.title("Calculator")
+window.resizable(False, False)
+
+frame = tkinter.Frame(window)
+label = tkinter.Label(frame, text="0", font=("Arial", 45), background = color_black,
+                      foreground=color_white)
+
+label.pack()
+
+
+window.mainloop()
